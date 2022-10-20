e6c823ff4aa7c0a438ffdecf421592a1f32580b6
hayoung10 <mhymsy@gmail.com>
2022-10-20 17:51:59 +0900

    server init

---
+++ server/src/main/java/com/codestates/Pre038Application.java
@@ -0,0 +1,13 @@
+package com.codestates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pre038Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pre038Application.class, args);
+	}
+
+}
