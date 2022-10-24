0202ef7d8b3ed0278c9b7a8b4be56693469ddf93
journey <public.jinhui.kim@gmail.com>
2022-10-24 21:23:18 +0900

    fix: router 수정

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
-import Main from "./pages/Main";
+import LandingPage from "./pages/LandingPage/LandingPage";
 
 const App = () => {
+  const [isLogin, setIsLogin] = useState(false);
+  useEffect(() => {
+    setIsLogin(false);
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Main />} />
+        <Route path="/" element={isLogin ? null : <LandingPage />} />
+        <Route path="*" element={<NoMatch />} />
       </Routes>
     </BrowserRouter>
   );
 };
 
 export default App;
+
+const NoMatch = () => {
+  return (
+    <div>
+      <h2>Nothing to see here!</h2>
+      <p>
+        <Link to="/">Go to the home page</Link>
+      </p>
+    </div>
+  );
+};
