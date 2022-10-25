64785295a65076c3ccaef001a907e7a797187206
jwo0o0 <jwo0o0358@gmail.com>
2022-10-25 15:47:40 +0900

    fix: Prodoucst 버튼 추가

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -5,10 +5,16 @@ const HeaderContinaer = styled.div`
     height: 47px;
     border-top: 3px solid #F48225;
     background-color: #F8F9F9;
+    display: flex;
+    align-items: center;
 `;
 
 const LogoContainer = styled.div`
+    height: 100%;
+    display: flex;
+    align-items: center;
     padding: 0px 8px;
+
     .logo-img {
         width: 150px;
         height: 30px;
@@ -16,6 +22,29 @@ const LogoContainer = styled.div`
         background-position-x: 0px;
         background-position-y: -500px;
         border-image-slice: 100%;
+    }
+
+    &:hover {
+        background-color: #E3E5E8;
+    }
+`
+
+const ProductsBtnContainer = styled.div`
+    width: 82px;
+    height: 33px;
+
+    .product_btn {
+        width: 78px;
+        height: 29px;
+        background-color: #F8F9F9;
+        color: #525960;
+        font-size: 13px;
+        font-weight: 400;
+        border: none;
+
+        &:hover {
+            background-color: #E3E5E8;
+        }
     }
 `
 
@@ -25,6 +54,9 @@ const Header = () => {
             <LogoContainer>
                 <a href='https://stackoverflow.com/'><div className='logo-img'></div></a>
             </LogoContainer>
+            <ProductsBtnContainer>
+                <button className='product_btn'>Products</button>
+            </ProductsBtnContainer>
         </HeaderContinaer>
     )
 }
