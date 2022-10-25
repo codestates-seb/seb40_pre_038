9cfd2412ec55bdaf69a5f96c34aa734b38a4259c
journey <public.jinhui.kim@gmail.com>
2022-10-26 00:13:29 +0900

    feat: 버튼 컴포넌트 추가

--- client/src/components/Buttons.js
+++ client/src/components/Buttons.js
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
 const Bluebutton = styled.button`
@@ -50,4 +51,61 @@ function ButtonSblue({ children, width, height, fontSize }) {
   );
 }
 
-export { ButtonBlue, ButtonSblue };
+function Button({ children, type }) {
+  const [color, setColor] = useState('');
+  const [backgroundColor, setbackgroundColor] = useState('');
+
+  useEffect(() => {
+    switch (type) {
+      case 'primary':
+        setColor('#ffffff');
+        setbackgroundColor('#0A95FF');
+        break;
+      default:
+        setColor('#0074CC');
+        setbackgroundColor('transparent');
+        break;
+    }
+  }, [type]);
+
+  return (
+    <ButtonComponent color={color} backgroundColor={backgroundColor}>
+      {children}
+    </ButtonComponent>
+  );
+}
+
+const ButtonComponent = styled.button`
+  position: relative;
+  display: inline-block;
+  padding: 0.8em;
+  color: ${(props) => props.color};
+  border: 1px solid transparent;
+  border-radius: 3px;
+  background-color: ${(props) => props.backgroundColor};
+  outline: none;
+  font-family: inherit;
+  font-size: 13px;
+  font-weight: normal;
+  line-height: calc((13 + 2) / 13);
+  text-align: center;
+  text-decoration: none;
+  cursor: pointer;
+  user-select: none;
+  box-shadow: inset 0 1px 0 0 hsl(0deg 0% 100% / 40%);
+
+  white-space: nowrap !important;
+
+  :hover,
+  :focus,
+  :active {
+    color: #ffffff;
+    background-color: #0074cc;
+  }
+  :active {
+    background-color: #0063bf;
+    box-shadow: none;
+  }
+`;
+
+export { ButtonBlue, ButtonSblue, Button };
