89696abdfb33a26a80fd05269f2d3e0c271ce4c4
jwo0o0 <jwo0o0358@gmail.com>
2022-10-25 15:49:44 +0900

    feat: Searchbar 컴포넌트 생성

---
+++ client/src/components/Searchbar.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const SearchbarContainer = styled.div`
+`
+
+const Searchbar = () => {
+    return (
+        <SearchbarContainer></SearchbarContainer>
+    )
+}
+
+export default Searchbar;
