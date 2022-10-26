4c0c5ddffd22e66da8116a64cfd061160d4325a2
jwo0o0 <jwo0o0358@gmail.com>
2022-10-26 15:25:51 +0900

    [FE] fix: Footer 반응형 1차 구현

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -9,13 +9,15 @@ const FooterContainer = styled.div`
   padding: 32px 12px 12px;
   display: flex;
 
+  @media screen and (min-width: 641px) {
+    //641 ~ 980px
+    height: 394.88px;
+    padding: 16px;
+    flex-direction: column;
+  }
   @media screen and (min-width: 981px) {
     //981px ~
     justify-content: center;
-  }
-  @media screen and (min-width: 641px) {
-    //641 ~ 980px
-    flex-direction: column;
   }
 `;
 
@@ -39,6 +41,31 @@ const SiteFooterTitleContainer = styled.div`
   margin-bottom: 12px;
   &:hover {
     cursor: pointer;
+  }
+`;
+
+const SiteFooters = styled.div`
+  display: flex;
+
+  @media screen and (min-width: 641px) {
+    //641 ~ 980px
+    flex-direction: column;
+  }
+  @media screen and (min-width: 981px) {
+    //981px ~
+    flex-direction: row;
+  }
+`;
+
+const SiteFooterLists = styled.div`
+  display: flex;
+  @media screen and (min-width: 641px) {
+    //641 ~ 980px
+    flex-direction: row;
+  }
+  @media screen and (min-width: 981px) {
+    //981px ~
+    flex-direction: column;
   }
 `;
 
@@ -114,40 +141,52 @@ const Footer = () => {
           />
         </svg>
       </FooterLogoContainer>
-      <StackFooterContainer>
-        <SiteFooterTitleContainer>STACK OVERFLOW</SiteFooterTitleContainer>
-        <SiteFooterListContainer>Questions</SiteFooterListContainer>
-        <SiteFooterListContainer>Help</SiteFooterListContainer>
-      </StackFooterContainer>
-      <ProductsFooterContainer>
-        <SiteFooterTitleContainer>PRODUCTS</SiteFooterTitleContainer>
-        <SiteFooterListContainer>Teams</SiteFooterListContainer>
-        <SiteFooterListContainer>Advertising</SiteFooterListContainer>
-        <SiteFooterListContainer>Collectives</SiteFooterListContainer>
-        <SiteFooterListContainer>Talent</SiteFooterListContainer>
-      </ProductsFooterContainer>
-      <CompanyFooterContainer>
-        <SiteFooterTitleContainer>COMPANY</SiteFooterTitleContainer>
-        <SiteFooterListContainer>About</SiteFooterListContainer>
-        <SiteFooterListContainer>Press</SiteFooterListContainer>
-        <SiteFooterListContainer>Work Here</SiteFooterListContainer>
-        <SiteFooterListContainer>Legal</SiteFooterListContainer>
-        <SiteFooterListContainer>Privacy Policy</SiteFooterListContainer>
-        <SiteFooterListContainer>Terms of Service</SiteFooterListContainer>
-        <SiteFooterListContainer>Contact Us</SiteFooterListContainer>
-        <SiteFooterListContainer>Cookie Settings</SiteFooterListContainer>
-        <SiteFooterListContainer>Cookie Policy</SiteFooterListContainer>
-      </CompanyFooterContainer>
-      <ExchangeFooterContainer>
-        <SiteFooterTitleContainer>Technology</SiteFooterTitleContainer>
-        <SiteFooterListContainer>Culture & recreation</SiteFooterListContainer>
-        <SiteFooterListContainer>Life & arts</SiteFooterListContainer>
-        <SiteFooterListContainer>Science</SiteFooterListContainer>
-        <SiteFooterListContainer>Professional</SiteFooterListContainer>
-        <SiteFooterListContainer>Business</SiteFooterListContainer>
-        <SiteFooterListContainer>API</SiteFooterListContainer>
-        <SiteFooterListContainer>Data</SiteFooterListContainer>
-      </ExchangeFooterContainer>
+      <SiteFooters>
+        <StackFooterContainer>
+          <SiteFooterTitleContainer>STACK OVERFLOW</SiteFooterTitleContainer>
+          <SiteFooterLists>
+            <SiteFooterListContainer>Questions</SiteFooterListContainer>
+            <SiteFooterListContainer>Help</SiteFooterListContainer>
+          </SiteFooterLists>
+        </StackFooterContainer>
+        <ProductsFooterContainer>
+          <SiteFooterTitleContainer>PRODUCTS</SiteFooterTitleContainer>
+          <SiteFooterLists>
+            <SiteFooterListContainer>Teams</SiteFooterListContainer>
+            <SiteFooterListContainer>Advertising</SiteFooterListContainer>
+            <SiteFooterListContainer>Collectives</SiteFooterListContainer>
+            <SiteFooterListContainer>Talent</SiteFooterListContainer>
+          </SiteFooterLists>
+        </ProductsFooterContainer>
+        <CompanyFooterContainer>
+          <SiteFooterTitleContainer>COMPANY</SiteFooterTitleContainer>
+          <SiteFooterLists>
+            <SiteFooterListContainer>About</SiteFooterListContainer>
+            <SiteFooterListContainer>Press</SiteFooterListContainer>
+            <SiteFooterListContainer>Work Here</SiteFooterListContainer>
+            <SiteFooterListContainer>Legal</SiteFooterListContainer>
+            <SiteFooterListContainer>Privacy Policy</SiteFooterListContainer>
+            <SiteFooterListContainer>Terms of Service</SiteFooterListContainer>
+            <SiteFooterListContainer>Contact Us</SiteFooterListContainer>
+            <SiteFooterListContainer>Cookie Settings</SiteFooterListContainer>
+            <SiteFooterListContainer>Cookie Policy</SiteFooterListContainer>
+          </SiteFooterLists>
+        </CompanyFooterContainer>
+        <ExchangeFooterContainer>
+          <SiteFooterTitleContainer>Technology</SiteFooterTitleContainer>
+          <SiteFooterLists>
+            <SiteFooterListContainer>
+              Culture & recreation
+            </SiteFooterListContainer>
+            <SiteFooterListContainer>Life & arts</SiteFooterListContainer>
+            <SiteFooterListContainer>Science</SiteFooterListContainer>
+            <SiteFooterListContainer>Professional</SiteFooterListContainer>
+            <SiteFooterListContainer>Business</SiteFooterListContainer>
+            <SiteFooterListContainer>API</SiteFooterListContainer>
+            <SiteFooterListContainer>Data</SiteFooterListContainer>
+          </SiteFooterLists>
+        </ExchangeFooterContainer>
+      </SiteFooters>
       <CopyrightFooterContainer>
         <SocialListContainer>
           <li>Blog</li>
