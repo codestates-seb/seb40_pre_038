b8118eebf6a929eb3209cd81d419aa1232e416dc
journey <public.jinhui.kim@gmail.com>
2022-10-27 02:31:15 +0900

    [FE] style: Top Questions - summury states
    - answers 답변여부, 채택여부에 따라 css 변경되도록 수정
    - views 수에 따른 css 변경

--- client/src/pages/Home/TopQuestionsPage/QuestionList/SummaryStats.js
+++ client/src/pages/Home/TopQuestionsPage/QuestionList/SummaryStats.js
@@ -1,18 +1,71 @@
+import { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
-const SummaryStats = () => {
+const SummaryStats = ({
+  votes = 0,
+  answers = 0,
+  hasAccepted = false,
+  views = 10000,
+}) => {
+  const [viewsClassName, setViewsClassName] = useState('');
+  const [viewsText, setViewsText] = useState(views);
+
+  useEffect(() => {
+    if (views < 1000) {
+      setViewsClassName('');
+      setViewsText(views);
+      return;
+    }
+
+    const k = Math.floor(views / 1000);
+    if (views < 10000) {
+      setViewsClassName('is-warm');
+      setViewsText(`${k}k`);
+      return;
+    }
+
+    setViewsClassName('is-hot');
+    if (views < 1000000) {
+      setViewsText(`${k}k`);
+      return;
+    }
+
+    const m = Math.floor(k / 1000);
+    if (views < 1000000000) {
+      setViewsText(`${m}m`);
+      return;
+    }
+
+    const b = Math.floor(m / 1000);
+    setViewsText(`${b}b`);
+  }, [views]);
+
   return (
     <SummaryStatsWrapper>
-      <SummaryStatsItem className="emphasized" title="Score of 0">
-        <SummaryStatsItemNumber>0</SummaryStatsItemNumber>
+      <SummaryStatsItem className="emphasized" title={`Score of ${votes}`}>
+        <SummaryStatsItemNumber>{votes}</SummaryStatsItemNumber>
         <SummaryStatsItemUnit>votes</SummaryStatsItemUnit>
       </SummaryStatsItem>
-      <SummaryStatsItem title="0 answers">
-        <SummaryStatsItemNumber>0</SummaryStatsItemNumber>
+      <SummaryStatsItem
+        className={
+          answers <= 0
+            ? ''
+            : hasAccepted
+            ? 'has-accepted-answer'
+            : 'has-answers'
+        }
+        title={`${answers} answers`}
+      >
+        {answers <= 0 ? null : (
+          <HasAcceptedAnswerSvg aria-hidden="true" viewBox="0 0 14 14">
+            <path d="M13 3.41 11.59 2 5 8.59 2.41 6 1 7.41l4 4 8-8Z"></path>
+          </HasAcceptedAnswerSvg>
+        )}
+        <SummaryStatsItemNumber>{answers}</SummaryStatsItemNumber>
         <SummaryStatsItemUnit>answers</SummaryStatsItemUnit>
       </SummaryStatsItem>
-      <SummaryStatsItem title="3 views">
-        <SummaryStatsItemNumber>3</SummaryStatsItemNumber>
+      <SummaryStatsItem className={viewsClassName} title={`${views} views`}>
+        <SummaryStatsItemNumber>{viewsText}</SummaryStatsItemNumber>
         <SummaryStatsItemUnit>views</SummaryStatsItemUnit>
       </SummaryStatsItem>
     </SummaryStatsWrapper>
@@ -51,6 +104,26 @@ const SummaryStatsItem = styled.div`
   &.emphasized {
     color: #0c0d0e;
   }
+  &.has-answers {
+    color: #2f6f44;
+  }
+  &.has-accepted-answer {
+    color: #fff;
+    background-color: #2f6f44;
+    border-color: #2f6f44;
+  }
+  &.has-answers,
+  &.has-accepted-answer {
+    border: 1px solid #2f6f44;
+    border-radius: 3px;
+    padding: 2px 4px;
+  }
+  &.is-warm {
+    color: #83690b;
+  }
+  &.is-hot {
+    color: #a7510c;
+  }
 `;
 
 const SummaryStatsItemNumber = styled.span`
@@ -58,3 +131,13 @@ const SummaryStatsItemNumber = styled.span`
 `;
 
 const SummaryStatsItemUnit = styled.span``;
+
+const HasAcceptedAnswerSvg = styled.svg`
+  vertical-align: bottom;
+  width: 14px;
+  height: 14px;
+  overflow: hidden;
+  path {
+    fill: currentColor;
+  }
+`;
