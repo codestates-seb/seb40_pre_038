cbf5a60420d0169f6da8d5904769badb608b76ba
jwo0o0 <jwo0o0358@gmail.com>
2022-10-26 13:13:11 +0900

    [FE] feat: Footer 컴포넌트 생성

---
+++ client/src/components/Footer.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+const FooterContainer = styled.div`
+  width: 100%;
+  height: 322px;
+  position: fixed;
+  bottom: 0;
+`;
+
+const Footer = () => {
+  return <FooterContainer></FooterContainer>;
+};
+
+export default Footer;
