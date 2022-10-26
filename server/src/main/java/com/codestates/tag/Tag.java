f7090453d8f97dd48aca90968de336feb9a31526
gnidinger <gcnb87@gmail.com>
2022-10-26 19:36:17 +0900

    [BE]fix/Reply[3]

--- server/src/main/java/com/codestates/tag/Tag.java
+++ server/src/main/java/com/codestates/tag/Tag.java
@@ -19,7 +19,7 @@ public class Tag {
     private long tagId;
 
     @Column(nullable = false)
-    private String name;
+    private String title;
 
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, name = "status")
