ccc522c9f8e8fa85618c9361e03b882e0f2cdfd1
journey <public.jinhui.kim@gmail.com>
2022-10-27 13:12:33 +0900

    [FE] chore: proxy 세팅
    - add module http-proxy-middleware
    - setupProxy.js 추가

---
+++ client/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function (app) {
+  app.use(
+    ['/questions', '/answer', '/search', '/tags', '/users', '/members'],
+    createProxyMiddleware({
+      target: 'http://localhost:8080', // 추후 env로 관리
+      changeOrigin: true,
+    })
+  );
+};
