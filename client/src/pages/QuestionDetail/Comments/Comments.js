4783ed2f6b187df404f4399e504906435f9b5ef1
jwo0o0 <jwo0o0358@gmail.com>
2022-10-31 12:52:05 +0900

    [FE] fix: 날짜 표시 수정

--- client/src/pages/QuestionDetail/Comments/Comments.js
+++ client/src/pages/QuestionDetail/Comments/Comments.js
@@ -8,17 +8,29 @@ const CommentsContainer = styled.div`
   margin-top: 35px;
 `;
 
+const createDate = (str) => {
+  const date = new Date(str.slice(0, 19));
+  const arr = date.toDateString().split(' ');
+
+  return `${arr[1]} ${arr[2]}, ${
+    arr[3]
+  } at ${date.getHours()}:${date.getMinutes()}`;
+};
+
 const Comments = ({ data }) => {
   const [commentData] = useState(data);
+
   return (
     <CommentsContainer>
       {commentData.map((comment) => {
+        const date = createDate(comment.createdAt);
+        console.log(date);
         return (
           <Comment
             key={comment.commentId}
             body={comment.body}
             nickname={comment.memberResponseDto.nickName}
-            date={comment.createdAt}
+            date={date}
           ></Comment>
         );
       })}
