e5adf0c328c91c402c203dbd5ccad35d9ab439f9
serin-B <bangserin980302@gmail.com>
2022-11-01 16:40:18 +0900

    [FE] fix: 식별자 이름 POST_NEW_QUESTION으로 변경

--- client/src/_actions/questions_action.js
+++ client/src/_actions/questions_action.js
@@ -1,21 +1,17 @@
 import axios from 'axios';
 import { QUESTIONS_ADD_URL } from '../api/requests';
 
-export const POST_QUESTION = 'POST_QUESTION';
-const BASE_URL = 'http://localhost:3000/';
+export const POST_NEW_QUESTION = 'POST_NEW_QUESTION';
 
-export const postQuestion = (postBody) => {
+export const postNewQuestion = (postBody) => {
   const request = axios
     .post(QUESTIONS_ADD_URL, postBody)
-    .then(function () {
-      window.location.href = BASE_URL;
-    })
     .catch(function (error) {
       console.log(error);
     });
 
   return {
-    type: POST_QUESTION,
+    type: POST_NEW_QUESTION,
     payload: request,
   };
 };
