9edf85a4ef0dc896245f0313468ea856f23e8583
journey <public.jinhui.kim@gmail.com>
2022-11-02 11:56:48 +0900

    [FE] Pagination 컴포넌트 antd 적용
    - npm i -f antd

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import GlobalStyle from './GlobalStyle';
+import 'antd/dist/antd.css';
 
 import Header from './components/Header';
 import HomePage from './pages/HomePage/HomePage/HomePage';
--- client/src/components/Pagination.js
+++ client/src/components/Pagination.js
@@ -1,10 +1,21 @@
 import styled from 'styled-components';
+import { Pagination as AntdPagination } from 'antd';
+
+const PagerWrapper = styled.div`
+  margin: 20px 0;
+  display: flex;
+  flex-wrap: wrap;
+  float: left !important;
+`;
 
 export const Pagination = ({ size, totalPages, currentPage }) => {
   return (
     <>
       <PageSizer size={size} />
-      <Pager totalPages={totalPages} currentPage={currentPage} />
+      <PagerWrapper>
+        <Pager totalPages={totalPages} currentPage={currentPage} />
+        {/* <TempPager totalPages={totalPages} currentPage={currentPage} /> */}
+      </PagerWrapper>
     </>
   );
 };
@@ -88,60 +99,97 @@ export const PageSizer = ({ size = 15 }) => {
   );
 };
 
-const PagerWrapper = styled.div`
-  margin: 20px 0;
-  display: flex;
-  flex-wrap: wrap;
-  float: left !important;
-`;
-
-export const Pager = ({ totalPages = 4, currentPage = 1 }) => {
-  const handleClickPrevNextButton = (e) => {
-    console.log(e.target.textContent);
+export const Pager = ({ totalPages = 1, currentPage = 1 }) => {
+  const itemRender = (_, type, originalElement) => {
+    if (type === 'prev')
+      return currentPage === 1 ? null : (
+        <PagenationButton title="Go to prev page">Prev</PagenationButton>
+      );
+    if (type === 'next')
+      return currentPage === totalPages ? null : (
+        <PagenationButton rel="next" title="Go to next page">
+          Next
+        </PagenationButton>
+      );
+    return originalElement;
   };
 
-  const handleClickPageNumberButton = (e) => {
-    console.log(e.target.textContent);
+  const onChangePage = (page) => {
+    console.log(page);
   };
 
   return (
-    <PagerWrapper>
-      {currentPage === 1 ? null : (
-        <>
-          <PagenationButton
-            title="Go to prev page"
-            onClick={handleClickPrevNextButton}
-          >
-            Prev
-          </PagenationButton>
-        </>
-      )}
-      <PagenationButton
-        title="Go to page 1"
-        onClick={handleClickPageNumberButton}
-      >
-        1
-      </PagenationButton>
-
-      {totalPages === 1 ? null : (
-        <PagenationButton
-          className={currentPage === totalPages ? 'is-selected' : ''}
-          title={`Go to page ${totalPages}`}
-        >
-          {totalPages}
-        </PagenationButton>
-      )}
-      {currentPage === totalPages ? null : (
-        <>
-          <PagenationButton
-            rel="next"
-            title="Go to next page"
-            onClick={handleClickPrevNextButton}
-          >
-            Next
-          </PagenationButton>
-        </>
-      )}
-    </PagerWrapper>
+    <AntdPagination
+      defaultPageSize={1}
+      defaultCurrent={currentPage}
+      total={totalPages}
+      itemRender={itemRender}
+      onChange={onChangePage}
+      showSizeChanger={false}
+    />
   );
 };
+
+/**
+ * 미완성 컴포넌트입니다.
+ * @param {*} param0
+ * @returns
+ */
+// const TempPager = ({ totalPages = 1, currentPage = 1 }) => {
+//   console.log(totalPages, currentPage);
+//   const start = currentPage - 2;
+//   const stop = currentPage + 2;
+//   const ellipsisButton = (
+//     <PagenationButton className="clear">…</PagenationButton>
+//   );
+//   const isStartEllipsis = totalPages > 6 && start > 2;
+//   const isStopEllipsis = totalPages > 6 && stop < totalPages - 1;
+//   const siblingCount = isStartEllipsis && isStopEllipsis ? 5 : 0; // stop - start + 1;
+
+//   const handleClickPrevNextButton = (e) => {
+//     console.log(e.target.textContent);
+//   };
+
+//   const handleClickPageNumberButton = (e) => {
+//     console.log(e.target.textContent);
+//   };
+
+//   return (
+//     <>
+//       {currentPage === 1 ? null : (
+//         <PagenationButton
+//           title="Go to prev page"
+//           onClick={handleClickPrevNextButton}
+//         >
+//           Prev
+//         </PagenationButton>
+//       )}
+//       <PagenationButton
+//         title="Go to page 1"
+//         onClick={handleClickPageNumberButton}
+//       >
+//         1
+//       </PagenationButton>
+//       {isStartEllipsis ? ellipsisButton : null}
+//       {Array.from({ length: siblingCount }, (_, i) => start + i)}
+//       {isStopEllipsis ? ellipsisButton : null}
+//       {totalPages === 1 ? null : (
+//         <PagenationButton
+//           className={currentPage === totalPages ? 'is-selected' : ''}
+//           title={`Go to page ${totalPages}`}
+//         >
+//           {totalPages}
+//         </PagenationButton>
+//       )}
+//       {currentPage === totalPages ? null : (
+//         <PagenationButton
+//           rel="next"
+//           title="Go to next page"
+//           onClick={handleClickPrevNextButton}
+//         >
+//           Next
+//         </PagenationButton>
+//       )}
+//     </>
+//   );
+// };
