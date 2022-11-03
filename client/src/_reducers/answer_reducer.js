d6018c0d5d34c06f0716fb1e1d1621d516c11646
jwo0o0 <jwo0o0358@gmail.com>
2022-11-03 22:42:26 +0900

    [FE] fix: Answer comment 추가 기능 구현

--- client/src/_reducers/answer_reducer.js
+++ client/src/_reducers/answer_reducer.js
@@ -8,6 +8,7 @@ const initialState = {
   data: [],
 };
 export const answerReducer = (state = initialState, action) => {
+  let changeAnswer;
   switch (action.type) {
     case GET_ANSWERS:
       return { ...state, ...action.payload };
@@ -15,7 +16,20 @@ export const answerReducer = (state = initialState, action) => {
       return { ...state, ...action.payload };
     case POST_ANSWER_COMMENT:
       console.log(action.payload);
-      return state;
+      console.log(state.data);
+      changeAnswer = state.data.filter((el) => {
+        return el.answerId === action.payload.answer_id;
+      });
+      console.log(changeAnswer);
+      changeAnswer[0].commentsWithUser.push(action.payload.data.data);
+      return {
+        ...state,
+        ...state.data.map((el) => {
+          if (el.answerId === action.payload.answer_id) return changeAnswer;
+          else return el;
+        }),
+      };
+    //return state;
     default:
       return state;
   }
--- client/src/api/requests.js
+++ client/src/api/requests.js
@@ -1,6 +1,6 @@
 export const USERS_URL = '/api/users';
 export const QUESTIONS_URL = '/api/questions';
-export const ANSWER_URL = '/api/answer';
+export const ANSWER_URL = '/api/answers';
 export const SEARCH_URL = '/api/search';
 export const TAGS_URL = '/api/tags';
 
