ba75b2b29e15ae78e6dd3731002595b6025a1f70
serin-B <bangserin980302@gmail.com>
2022-11-04 15:36:23 +0900

    [FE] fix: proxy /api/answers 수정 및 서버주소 하드코딩

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -2,9 +2,16 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
   app.use(
-    ['/api/questions', '/api/answer', '/api/search', '/api/tags', '/api/users'],
+    [
+      '/api/questions',
+      '/api/answers',
+      '/api/search',
+      '/api/tags',
+      '/api/users',
+    ],
     createProxyMiddleware({
-      target: 'http://localhost:8080', // 추후 env로 관리
+      target:
+        'http://ec2-13-125-208-244.ap-northeast-2.compute.amazonaws.com:8080', // 추후 env로 관리
       changeOrigin: true,
     })
   );
