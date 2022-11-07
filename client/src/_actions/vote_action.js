5878fec7fd238319feb926d7f1a5069c1c7455c6
serin-B <bangserin980302@gmail.com>
2022-11-07 12:04:02 +0900

    [FE] patchAnswersVote 로그인 정보 헤더에 추가

--- client/src/_actions/vote_action.js
+++ client/src/_actions/vote_action.js
@@ -21,7 +21,11 @@ export const patchQuestionsVote = async (question_id, patchBody) => {
 
 export const patchAnswersVote = async (answers_id, patchBody) => {
   const payload = await axios
-    .patch(`${ANSWER_URL}/${answers_id}/vote`, patchBody)
+    .patch(`${ANSWER_URL}/${answers_id}/vote`, patchBody, {
+      headers: {
+        Authorization: sessionStorage.Authorization,
+      },
+    })
     .then((res) => res)
     .catch((error) => console.error(error));
   return {
