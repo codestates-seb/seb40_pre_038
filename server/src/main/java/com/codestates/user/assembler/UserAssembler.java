3db0e9a7f90b4f2749163d2d3b32f3be3d979a59
Jade Kim <JadeKim.dev@gmail.com>
2022-11-07 12:58:07 +0900

    [BE] fix: User refactoring

--- server/src/main/java/com/codestates/user/assembler/UserAssembler.java
+++ server/src/main/java/com/codestates/user/assembler/UserAssembler.java
@@ -1,7 +1,7 @@
 package com.codestates.user.assembler;
 
-import com.codestates.user.entity.User;
 import com.codestates.user.controller.UserController;
+import com.codestates.user.dto.UserDto;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.server.RepresentationModelAssembler;
 import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
@@ -11,12 +11,12 @@ import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
 import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @Component
-public class UserAssembler implements RepresentationModelAssembler <User, EntityModel<User>> {
+public class UserAssembler implements RepresentationModelAssembler <UserDto.Response, EntityModel<UserDto.Response>> {
 
     @Override
-    public EntityModel<User> toModel(User user) {
-        return EntityModel.of(user,
-                WebMvcLinkBuilder.linkTo(methodOn(UserController.class).getUser(user.getUserId())).withSelfRel(),
+    public EntityModel<UserDto.Response> toModel(UserDto.Response response) {
+        return EntityModel.of(response,
+                WebMvcLinkBuilder.linkTo(methodOn(UserController.class).getUser(response.getUserId())).withSelfRel(),
                 linkTo(methodOn(UserController.class).getUsers()).withRel("users"));
     }
 
