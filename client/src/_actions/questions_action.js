ddd19bfb81bc11e9cd947d99e0c7fd1508624aca
serin-B <bangserin980302@gmail.com>
2022-11-07 11:55:54 +0900

    [FE] fix: sessionStorage 에서 유저 로그인 정보 헤더에 추가

--- client/src/_actions/questions_action.js
+++ client/src/_actions/questions_action.js
@@ -4,8 +4,13 @@ import { QUESTIONS_ADD_URL } from '../api/requests';
 export const POST_NEW_QUESTION = 'POST_NEW_QUESTION';
 
 export const postNewQuestion = async (postBody) => {
+  const sessionStorage = window.sessionStorage;
   const request = await axios
-    .post(QUESTIONS_ADD_URL, postBody)
+    .post(QUESTIONS_ADD_URL, postBody, {
+      headers: {
+        Authorization: sessionStorage.Authorization,
+      },
+    })
     .catch(function (error) {
       console.log(error);
     });
